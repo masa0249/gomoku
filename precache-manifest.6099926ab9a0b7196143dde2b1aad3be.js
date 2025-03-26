93bc202bbfa5e9256ef98271e7ff2a9591e62a22
Masato Taniguchi <masatotaniguchi@MasatonoMacBook-Pro.local>
2025-03-26 22:26:52 +0900

    Updates

---
+++ precache-manifest.6099926ab9a0b7196143dde2b1aad3be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1318f0321740e0376d51498117177c99",
+    "url": "/masa0249/gomoku.git/index.html"
+  },
+  {
+    "revision": "52d128a18b0ca3621891",
+    "url": "/masa0249/gomoku.git/static/css/main.ee9c8ead.chunk.css"
+  },
+  {
+    "revision": "638e200e69d950262938",
+    "url": "/masa0249/gomoku.git/static/js/2.ae026ab9.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/masa0249/gomoku.git/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "52d128a18b0ca3621891",
+    "url": "/masa0249/gomoku.git/static/js/main.82509b12.chunk.js"
+  },
+  {
+    "revision": "14d393a381083664bd35",
+    "url": "/masa0249/gomoku.git/static/js/runtime~main.93b75463.js"
+  }
+]);
