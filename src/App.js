fce3395599dbfc0be48b15d5474bad3442c75952
Masato Taniguchi <masatotaniguchi@MasatonoMacBook-Pro.local>
2025-04-07 05:06:40 +0900

    一人対戦実装

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Board from "./components/GameBoard";
+import OneGameBoard from "./components/OneGameBoard";
+import TwoGameBoard from "./components/TwoGameBoard";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/game" element={<Board />} />
+      <Route path="/onegame" element={<OneGameBoard />} />
+      <Route path="/twogame" element={<TwoGameBoard />} />
     </Routes>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,10 +8,8 @@ const Home = () => {
     <div className="home-container">
       <h1>五目並べ</h1>
       <p>モードを選択してください</p>
-      <button onClick={() => alert("現在、一人用モードは準備中です")}>
-        一人用（CPU対戦）
-      </button>
-      <button onClick={() => navigate("/game")}>二人用（対人戦）</button>
+      <button onClick={() => navigate("/onegame")}>一人用（CPU 戦）</button>
+      <button onClick={() => navigate("/twogame")}>二人用（対人戦）</button>
     </div>
   );
 };
