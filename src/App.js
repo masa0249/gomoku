f81bf6b51135d18aa877463400354fcb1c23acc7
Masato Taniguchi <masatotaniguchi@MasatonoMacBook-Pro.local>
2025-04-09 05:42:02 +0900

    cpuにアクセス許可

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Board from "./components/GameBoard";
+import CpuBoard from "./components/CpuBoard";
+import GameBoard from "./components/GameBoard";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/game" element={<Board />} />
+      <Route path="/cpu" element={<CpuBoard />} />
+      <Route path="/game" element={<GameBoard />} />
     </Routes>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,7 @@ const Home = () => {
     <div className="home-container">
       <h1>五目並べ</h1>
       <p>モードを選択してください</p>
-      <button onClick={() => alert("現在、一人用モードは準備中です")}>
-        一人用（CPU対戦）
-      </button>
+      <button onClick={() => navigate("/cpu")}>一人用（CPU戦）</button>
       <button onClick={() => navigate("/game")}>二人用（対人戦）</button>
     </div>
   );
